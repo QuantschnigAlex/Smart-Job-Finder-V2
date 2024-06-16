a73a562b1b3157c6f08764934d52f53b8b84276a
Jovic <stefan.jovic@edu.fh-joanneum.at>
2024-06-16 15:45:32 +0200

    add mail keyboard to register field mail

--- app/src/main/java/com/example/smart_job_finder_v2/ui/screens/register/Register.kt
+++ app/src/main/java/com/example/smart_job_finder_v2/ui/screens/register/Register.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -89,6 +90,10 @@ fun RegisterScreen(
                     onValueChange = { viewModel.updateEmail(it) },
                     placeholder = { Text(stringResource(R.string.EmailPlaceholder)) },
                     trailingIcon = painterResource(id = R.drawable.email),
+                    keyboardOptions = KeyboardOptions.Default.copy(
+                        keyboardType = KeyboardType.Email,
+                        autoCorrect = false
+                    ),
                     modifier = Modifier.fillMaxWidth()
                 )
                 Spacer(modifier = Modifier.height(16.dp))
